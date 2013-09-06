2409bfd114d0a50c8e4c0fca88eb535d8c17f3f7
aaronchen2k <aaronchen2k@gmail.com>
2013-09-06 09:33:23 +0800

    change

--- public/admin/app/scripts/routes/application_route.js
+++ public/admin/app/scripts/routes/application_route.js
@@ -1,5 +1,7 @@
 Admin.ApplicationRoute = Ember.Route.extend({
     // admittedly, this should be in IndexRoute and not in the
     // top level ApplicationRoute; we're in transition... :-)
-
+    model: function () {
+        return ['red', 'yellow', 'blue'];
+    }
 });
