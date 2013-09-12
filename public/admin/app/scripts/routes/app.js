0106100992c026060a3abf71562d308f978a404f
aaronchen2k <aaronchen2k@gmail.com>
2013-09-12 14:21:54 +0800

    qi

--- public/admin/app/scripts/routes/app.js
+++ public/admin/app/scripts/routes/app.js
@@ -24,9 +24,12 @@ Admin.AppListRoute = Ember.Route.extend({
 
 Admin.AppEditRoute = Ember.Route.extend({
     model: function(params) {
+        if (Util.isNotEmpty(params.app_id)) {
+            var app = this.store.find('app', params.app_id);
+            return app;
+        }
     },
     setupController: function(controller, app) {
-        console.log(app);
         controller.set('model', app);
     }
 });
--- system/modules/app/contrl.js
+++ system/modules/app/contrl.js
@@ -5,10 +5,10 @@ var AppModel = mongoose.model('App', require('./model').App);
 
 //Rest Interface
 exports.findAll = function (req, res) {
-    return mongoose.findAll(AppModel, req, res);
+    mongoose.findAll(AppModel, req, res);
 };
 exports.findById = function(req, res){
-    return mongoose.findById(AppModel, req, res);
+    mongoose.findById(AppModel, req, res);
 };
 
 exports.save = function (req, res) {
