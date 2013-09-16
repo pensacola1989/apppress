fee882fb8d0ee2f58da3f1a67e7d6409bc264fa9
aaronchen2k <aaronchen2k@gmail.com>
2013-09-16 14:16:23 +0800

    qi

--- system/modules/module/model.js
+++ system/modules/module/model.js
@@ -1,10 +1,5 @@
 var mongoose = require('../../../framework/mongoose');
 
-var Keywords = new mongoose.Schema({
-    keyword: String
-});
-exports.Keywords = Keywords;
-
 var Module = new mongoose.Schema({
     id: String,
     code: String,
@@ -17,6 +12,25 @@ var Module = new mongoose.Schema({
     updateTime: Date
 });
 exports.Module = Module;
+
+var ModuleModel = mongoose.model('Module', Module);
+ModuleModel.find().exec(function (err, objs) {
+    if (!err) {
+        if (objs.length === 0) {
+            ModuleModel.create(
+                {code: 'album',  name: '相册', title: '相册', status: 1, createTime: new Date()},
+                {code: 'event',  name: '事件', title: '事件', status: 1, createTime: new Date()},
+                {code: 'video',  name: '视频', title: '视频', status: 1, createTime: new Date()},
+                {code: 'store',  name: '商店', title: '商店', status: 1, createTime: new Date()},
+                {code: 'coupon', name: '优惠券', title: '优惠券', status: 1, createTime: new Date()},
+                function (err, small) {
+                    if (err) return handleError(err);
+                        console.log('Init Module db successfully!!!');
+                }
+            )
+        }
+    }
+});
 
 
 
