b00925e2170df66ac29e5fe2d7170359b1d71a2c
aaronchen2k <aaronchen2k@gmail.com>
2013-11-15 14:06:13 +0800

    c

--- web/app/scripts/controllers/main.js
+++ web/app/scripts/controllers/main.js
@@ -6,5 +6,26 @@ angular.module('web')
     })
     .controller('MainCtrl', function ($scope) {
 
+    }).controller('CmsMenuCtrl', function ($scope) {
+        $scope.myInterval = 1000;
+        $scope.slides = $scope.slides = [];
+        for (var i = 0; i < 6; i += 5) {
+            var page = [];
+            for (var j = 0; j<5; j++) {
+                var index  = i + j;
+
+                page.push({
+                    image: 'http://placekitten.com/' + index + '/200',
+                    enabled: index > 5? false: true
+                });
+            }
+            $scope.slides.push(page);;
+        }
+
+        $scope.showClass = function(item) {
+            console.log(item);
+            if(item.enabled) return "";
+            else return "disabled";
+        }
     });
 
