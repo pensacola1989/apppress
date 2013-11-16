f2b9407142ca08b107615861330e5bec72225e44
aaronchen2k <aaronchen2k@gmail.com>
2013-11-16 18:24:05 +0800

    c

--- web/app/scripts/controllers/apps.js
+++ web/app/scripts/controllers/apps.js
@@ -2,15 +2,16 @@
 
 angular.module('web')
     .controller('AppListCtrl', function ($scope, $location) {
-        $scope.create = function(){
-            var url = '/app/edit/:111';
-            $location.path(url);
-        }
+//        $scope.create = function(){
+//            var url = '/app/edit/111';
+//            $location.path(url);
+//        }
     })
     .controller('AppDetailCtrl', function ($scope) {
 
     })
-    .controller('AppEditCtrl', function ($scope) {
-
-    });
+    .controller('AppEditCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
+        $scope.appId = $routeParams.appId;
+        console.log($scope.appId);
+    }]);
 
